79587555456328c8adc3af6ea5e62731d9bbd2ba
luannh <huuluan0201@gmail.com>
2021-05-28 09:29:43 +0700

    add three tower

--- some-maths/three-towers.go
+++ some-maths/three-towers.go
@@ -1,1 +1,35 @@
 package some_maths
+
+import "fmt"
+
+type solver interface {
+	play(int)
+}
+
+type towers struct {
+
+}
+
+func (t *towers) play(n int) {
+	t.moveN(n, 1,2,3)
+}
+
+func (t *towers) moveN(n, from, to, via int) {
+	if n > 0 {
+		t.moveN(n-1, from, via, to)
+		t.moveM(from, to)
+		t.moveN(n-1, via, to, from)
+	}
+}
+
+func (t *towers) moveM(from, to int) {
+	fmt.Println("Move disk from rod", from, "to rod", to)
+}
+
+
+//example
+//func main() {
+//	var t solver
+//	t = new(towers) // type towers must satisfy solver interface
+//	t.play(4)
+//}
--- some-sorts/binarysearch.go
+++ some-sorts/binarysearch.go
@@ -1,1 +1,21 @@
 package some_sorts
+
+func BinarySearch(needle int, haystack []int) bool {
+	low := 0
+	high := len(haystack) - 1
+	for low <= high {
+		median := (low + high) / 2
+
+		if haystack[median] < needle {
+			low = median + 1
+		} else {
+			high = median + 1
+		}
+	}
+
+	if low == len(haystack) || haystack[low] != needle {
+		return false
+	}
+
+	return true
+}
