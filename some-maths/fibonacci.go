d2ae5a1cf1f8651677a15bf1838e1266176906d2
huuluan97 <huuluan0201@gmail.com>
2021-06-02 23:28:19 +0700

    add math fibonacci

---
+++ some-maths/fibonacci.go
@@ -0,0 +1,16 @@
+package some_maths
+
+func MathFibonacci(n uint) uint {
+	if n == 0 {
+		return 0
+	}
+
+	var n1, n2 uint = 0,1
+
+	for i := uint(1); i < n; i++ {
+		n3 := n1 + n2
+		n1 = n2
+		n2 = n3
+	}
+	return n2
+}
